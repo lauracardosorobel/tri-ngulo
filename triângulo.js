aa05f6cb52531fc570f17e5b1171b5ed3edcffb0
lauracardosorobel <113368682+lauracardosorobel@users.noreply.github.com>
2023-10-18 09:34:10 -0300

    Add files via upload

---
+++ triângulo.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const Lado A= number (form.inLadoA.value)
+    const Lado B= number (form.inLadoB.value)
+    const Lado C= number (form.inLadoC.value)
+    let leve= permitida + (permitida * 0.2)
+
+    if(condutor <= permitida){
+        resp.innerText= 'Situação: Sem Multa'
+
+    } else if(condutor <= leve) {
+        resp.innerText= 'Situação: Multa leve'
+
+    } else {
+        resp.innerText= 'Situação: Multo grave'
+    
+    }
+})
